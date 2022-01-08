52088b7d5116079b23374267fdca95a77bf34d9f
niksm7 <nikhilsmankani@gmail.com>
2022-01-08 09:00:53 +0530

    logic to distribute

--- Brownie/contracts/KirshiOps.sol
+++ Brownie/contracts/KirshiOps.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract KirshiOps is Ownable,VRFConsumerBase {
-    uint256 public fee;
-    bytes32 public keyhash;
+contract KirshiOps is Ownable {
     uint256 private goods_counter = 777;
 
     Goods[] public all_goods;
@@ -17,6 +14,8 @@ contract KirshiOps is Ownable,VRFConsumerBase {
     mapping(uint256 => address[]) public goods_to_farmers;
     mapping(uint256 => uint256) public goods_to_availability;
     mapping(address => uint256[]) public donors_to_goods;
+    mapping(address => uint256[]) public farmer_to_allocations;
+    mapping(uint256 => uint256[]) public good_last_farmer_index;
 
     struct Goods {
         uint256 id;
@@ -24,16 +23,6 @@ contract KirshiOps is Ownable,VRFConsumerBase {
         uint256 token_amount;
         string image_uri;
         string description;
-    }
-
-    constructor(
-        address _vrfCoordinator,
-        address _link,
-        uint256 _fee,
-        bytes32 _keyhash
-    )public VRFConsumerBase(_vrfCoordinator, _link){
-        fee = _fee;
-        keyhash = _keyhash;
     }
 
     function addGoods(
@@ -51,35 +40,98 @@ contract KirshiOps is Ownable,VRFConsumerBase {
         );
         id_to_good[goods_counter] = new_good;
         all_goods.push(new_good);
-        goods_counter ++;
+        goods_counter++;
     }
 
-
-    function requestDonation(
-        address farmer_address,
-        uint256[] memory good_ids
-    ) public{
+    function requestDonation(address farmer_address, uint256[] memory good_ids)
+        public
+    {
         for (uint256 index = 0; index < good_ids.length; index++) {
-            goods_to_farmers[good_ids[index]].push(farmer_address);
+            if(goods_to_availability[good_ids[index]] > 0){
+                farmer_to_allocations[farmer_address].push(good_ids[index]);
+                goods_to_availability[good_ids[index]]--;
+            }
+            else{
+                goods_to_farmers[good_ids[index]].push(farmer_address);
+            }
         }
     }
 
-
-    function placeDonation(
-        address donor_address,
-        uint256[] memory good_ids
-    ) public{
+    function placeDonation(address donor_address, uint256[] memory good_ids)
+        public
+    {
         for (uint256 index = 0; index < good_ids.length; index++) {
             donors_to_goods[donor_address].push(good_ids[index]);
             goods_to_availability[good_ids[index]]++;
         }
-
-        // Call random function
-        bytes32 requestId = requestRandomness(keyhash, fee);
     }
 
+    // Function to distribute the goods based on user donations
+    function distributeDonation(address donor_address) public onlyOwner {
+        for (uint256 good_index = 0; good_index < donors_to_goods[donor_address].length; good_index++) {
 
-    // Get Random number and perform actions
-    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override{
+            uint256 curr_good_id = donors_to_goods[donor_address][good_index];
+            uint256 available_good_quantity = goods_to_availability[curr_good_id];
+
+            // When there is no requests
+            if (goods_to_farmers[curr_good_id].length == 0) {
+                continue;
+            }
+            // When the available good quantity is more than requested
+            else if (available_good_quantity >= goods_to_farmers[curr_good_id].length) {
+                for (uint256 farmer_index = 0; farmer_index < goods_to_farmers[curr_good_id].length; farmer_index++) {
+                    farmer_to_allocations[goods_to_farmers[curr_good_id][farmer_index]].push(good_index);
+                    available_good_quantity--;
+                }
+                delete goods_to_farmers[curr_good_id];
+                good_last_farmer_index[curr_good_id][0] = 0;
+                good_last_farmer_index[curr_good_id][1] = 0;
+            } else {
+                uint256 cuur_farmer_index = good_last_farmer_index[curr_good_id][0];
+                uint256 reverse = 0;
+
+                // If we are ahead of last index and there are still elements left we reverse
+                if (cuur_farmer_index > goods_to_farmers[curr_good_id].length - 1 && goods_to_farmers[curr_good_id].length > 0) {
+                    reverse = 1;
+                    cuur_farmer_index = goods_to_farmers[curr_good_id].length - 1;
+                    good_last_farmer_index[curr_good_id][1] = 1;
+                }
+
+                if (reverse != 1 && good_last_farmer_index[curr_good_id][1] == 1) {
+                    reverse = 1;
+                }
+
+                while (available_good_quantity != 0) {
+                    farmer_to_allocations[goods_to_farmers[curr_good_id][cuur_farmer_index]].push(good_index);
+
+                    // Swap the allocated farmer with last member
+                    goods_to_farmers[curr_good_id][cuur_farmer_index] = goods_to_farmers[curr_good_id][goods_to_farmers[curr_good_id].length - 1];
+
+                    // Delete the last member
+                    goods_to_farmers[curr_good_id].pop();
+
+                    available_good_quantity--;
+
+                    if (reverse == 0) {
+                        cuur_farmer_index++;
+                    } else {
+                        cuur_farmer_index--;
+                    }
+
+                    // If we are ahead of last index and there are still elements left we reverse
+                    if (reverse != 1 && cuur_farmer_index > goods_to_farmers[curr_good_id].length - 1 && goods_to_farmers[curr_good_id].length > 0) {
+                        reverse = 1;
+                        cuur_farmer_index--;
+                        good_last_farmer_index[curr_good_id][1] = 1;
+                    }
+                }
+                good_last_farmer_index[curr_good_id][0] = cuur_farmer_index;
+            }
+
+            goods_to_availability[curr_good_id] = available_good_quantity;
+            delete donors_to_goods[donor_address][good_index];
+        }
+
+        delete donors_to_goods[donor_address];
     }
 }
