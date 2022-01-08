e41458177027a7e8c348d3f0a4083f5cf8c9ae64
aishux <aishux07@gmail.com>
2022-01-08 17:55:02 +0530

    buy tokens and template changes

--- krishiDaan/krishiDaan/urls.py
+++ krishiDaan/krishiDaan/urls.py
@@ -1,18 +1,3 @@
-"""krishiDaan URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -23,11 +8,12 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.loginpage, name='loginpage'),
+    path('profile/', views.profile, name='profile'),
     path('shop/', views.shop, name='shop'),
-    path('request/', views.requestGoods, name='requestGoods'),
-    path('profile/', views.donorProfile, name='donorProfile'),
-    path('pending/', views.pendingRequest, name='pendingRequest'),
     path('donations/', views.donations, name='donations'),
+    path('request/', views.requestGoods, name='requestGoods'),
+    path('pending/', views.pendingRequest, name='pendingRequest'),
+    path('allocations/', views.allocatedGoods, name='allocatedGoods'),
     # path('postsign/', views.postsign, name='postsign'),
     # path('logout/', views.logout, name='logout'),
     
--- krishiDaan/krishiDaan/views.py
+++ krishiDaan/krishiDaan/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, redirect
+import pyrebase
+from django.contrib import auth
 
 
 def loginpage(request):
   return render(request,'login.html') 
 
 def shop(request):
-  return render(request,'shop.html') 
+  return render(request,'donor/shop.html') 
 
 def requestGoods(request):
-  return render(request,'request-goods.html') 
+  return render(request,'farmer/request-goods.html') 
 
-def donorProfile(request):
-  return render(request,'donor-profile.html') 
+def profile(request):
+  return render(request,'profile.html') 
 
 def pendingRequest(request):
-  return render(request,'pending-req.html') 
+  return render(request,'farmer/pending-req.html') 
 
 def donations(request):
   return render(request,'donations.html') 
